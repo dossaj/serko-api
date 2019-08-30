7d1519aa326259ed1d6ec34b3432f0ae678d98ca
Jason Dos Santos <dossaj@users.noreply.github.com>
2019-08-31 11:29:36 +1200

    Feature: Transaction Support (#3)
    * include transaction support

--- src/Serko.Expense.Server/Middleware/TransactionMiddleware.cs
+++ src/Serko.Expense.Server/Middleware/TransactionMiddleware.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Transactions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Serko.Expense.Core;
 
 namespace Serko.Expense.Server.Middleware
 {
-    public class ScopeMiddleware : IMiddleware
+    public class TransactionMiddleware : IMiddleware
     {
         private readonly IScopeManager manager;
         private readonly IServiceProvider provider;
 
-        public ScopeMiddleware(IScopeManager manager, IServiceProvider provider)
+        public TransactionMiddleware(IScopeManager manager, IServiceProvider provider)
         {
             this.manager = manager;
             this.provider = provider;
@@ -19,10 +20,12 @@ namespace Serko.Expense.Server.Middleware
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            using (provider.CreateScope())
             using (manager.BeginScope())
+            using (provider.CreateScope())
+            using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
             {
                 await next(context);
+                scope.Complete();
             }
         }
     }
